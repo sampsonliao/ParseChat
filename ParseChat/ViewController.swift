157b4cf4444ba26558522e52ba1901ce1bf323a8
Sampson Liao <sampsonliao@sampsons-mac.local>
2019-02-27 14:10:52 -0800

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Sampson Liao on 2/27/19.
+//  Copyright © 2019 sliao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
